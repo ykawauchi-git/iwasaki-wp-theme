040b82b6bb0a56b8cafee0322d8b2473b4ad09bf
Tomoya <tomoya@heads-rep.com>
2023-05-11 12:57:58 +0900

    学生支援ページ（PC）

---
+++ content/page/support.php
@@ -0,0 +1,317 @@
+<?php $pageName = "page-support";?>
+<section class="<?php echo $pageName;?>">
+  <div class="<?php echo $pageName;?>__intro">
+  「子どもの夢を叶えたい、可能性を広げてあげたい」「でも学費が心配・・」そういったご家庭は決して少なくありません。岩崎学園は独自の奨学生制度や分納制度、またモチベーションアップのためのほう賞制度があります。その他、実習室はもちろん、体育館やテニスコート、フットサルコートなど様々な施設・設備が充実しているのは本学園ならではです。
+  </div>
+  <div class="<?php echo $pageName;?>__anchor">
+    <a href="#headquarters" class="btn__anchor">学費サポート</a>
+    <a href="#vocational_school" class="btn__anchor">特待生制度</a>
+    <a href="#graduate_university" class="btn__anchor">学生寮</a>
+    <a href="#kindergarten" class="btn__anchor">充実の施設・設備</a>
+  </div>
+  <!-- 岩崎学園独自の奨学財団 -->
+  <div class="<?php echo $pageName;?>__foundation">
+    <h2 class="<?php echo $pageName;?>__foundation-heading page-heading"><span>岩崎学園独自の奨学財団</span></h2>
+    <div class="<?php echo $pageName;?>__foundation-inner">
+      <div class="<?php echo $pageName;?>__foundation-item">
+        <div class="<?php echo $pageName;?>__foundation-ttl">一般財団法人 岩崎ともみ奨学財団</div>
+        <div class="<?php echo $pageName;?>__foundation-img">
+          <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_foundation01.jpg">
+        </div>
+        <div class="<?php echo $pageName;?>__foundation-txt">
+          一般財団法人 岩崎ともみ奨学財団は、「優れた資質を持ちながら経済的事由により就学が困難な学生に、職業教育を受けるための奨学金を給付する財団をつくりたい」という、岩崎学園 学園長、横浜fカレッジ 名誉学校長の岩崎ともみの遺志から、2016年1月に設立されました。「どうすれば人が喜ぶか。どうすれば教育を通じて社会に貢献できるか」ということをつねに考えていた岩崎ともみの思いを受け継ぎ、未来を創る人材の育成に寄与するために、本財団では、夢と意欲を持った若い世代へより多くの教育機会を提供してまいります。
+        </div>
+        <a href="<?php echo home_url('/about/history'); ?>" class="page-group__contBtn">詳しく見る</a>
+      </div>
+      <div class="<?php echo $pageName;?>__foundation-item">
+        <div class="<?php echo $pageName;?>__foundation-ttl">一般財団法人 岩崎幸雄IT教育・研究振興奨学財団</div>
+        <div class="<?php echo $pageName;?>__foundation-img">
+          <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_foundation01.jpg">
+        </div>
+        <div class="<?php echo $pageName;?>__foundation-txt">
+          一般財団法人 岩崎幸雄IT教育・研究振興奨学財団は、これからの高度情報社会を牽引する青少年をはじめ若い人材の育成に資するため、児童・生徒の情報活用能力の育成を図る教育の推進、学生・社会人に対する教育支援等を目的に平成30年12月に設立されました。<br>
+          本財団では、夢と意欲を持った若い世代へより多くの教育機会を提供してまいります。
+        </div>
+        <a href="<?php echo home_url('/about/history'); ?>" class="page-group__contBtn">詳しく見る</a>
+      </div>
+    </div>
+  </div>
+  <!-- 学費サポートPLAN -->
+  <div class="<?php echo $pageName;?>__plan">
+    <h2 class="<?php echo $pageName;?>__plan-heading page-heading"><span>学費サポートPLAN</span></h2>
+    <ul class="<?php echo $pageName;?>__plan-list">
+    <!-- 岩崎学園奨学生制度 -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">岩崎学園奨学生制度</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          本学園の専門学校に入学を希望し、学業成績、人物が優秀であり、自立心が旺盛であるが、経済的理由により学費が不足する方を対象に、希望者の中から選考の上、年間50万円の奨学金を貸与する制度です。
+        </div>
+        <ul class="<?php echo $pageName;?>__plan-subList">
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">募集時期</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">願書提出時、進級時</div>
+          </li>
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">実施校</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              情報科学専門学校、横浜医療情報専門学校、横浜ｆカレッジ、横浜デジタルアーツ専門学校、横浜リハビリテーション専門学校、横浜保育福祉専門学校、横浜実践看護専門学校
+            </div>
+          </li>
+        </ul>
+      </li>
+      <!-- 岩崎学園学費分割納入制度 -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">岩崎学園学費分割納入制度</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          高校在学中に日本学生支援機構の予約採用（入学前に奨学金を予約する制度）を受けられている方を対象とし、入学金を除く学費の納入を入学後、日本学生支援機構奨学金で月次分割納入する制度です。
+        </div>
+        <ul class="<?php echo $pageName;?>__plan-subList">
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">募集時期</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              予約採用の申込み期間は高校によって異なりますので、 詳しくは高校の先生にご確認ください。その後各校にて手続きを行います
+            </div>
+          </li>
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">実施校</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              情報科学専門学校、横浜医療情報専門学校、横浜ｆカレッジ、横浜デジタルアーツ専門学校、横浜リハビリテーション専門学校、横浜保育福祉専門学校、横浜実践看護専門学校
+            </div>
+          </li>
+        </ul>
+      </li>
+      <!-- ３・４年制学科特別推薦入学制度 -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">３・４年制学科特別推薦入学制度</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          3年制、4年制学科に入学を希望される方で、学校が定める条件に適する方を対象とした、入学試験選考料や入学金の一部の免除を受けることのできる制度です。※免除金額は各校によって異なります。
+        </div>
+        <ul class="<?php echo $pageName;?>__plan-subList">
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">募集時期</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              願書提出時
+            </div>
+          </li>
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">実施校</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              情報科学専門学校、横浜医療情報専門学校、横浜実践看護専門学校
+            </div>
+          </li>
+        </ul>
+      </li>
+      <!-- ほう賞制度 -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">ほう賞制度</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          学校が定める条件の下、高度な資格に合格された方や学内コンテストの入賞者を対象とした、学費の一部の免除や記念品の授与を受けることのできる制度です。
+        </div>
+        <ul class="<?php echo $pageName;?>__plan-subList">
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">募集時期</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              入学後、資格試験実施時。または学内コンテスト実施時。
+            </div>
+          </li>
+          <li class="<?php echo $pageName;?>__plan-subItem">
+            <div class="<?php echo $pageName;?>__plan-subTtl">実施校</div>
+            <div class="<?php echo $pageName;?>__plan-subTxt">
+              情報科学専門学校、横浜医療情報専門学校、横浜ｆカレッジ、横浜デジタルアーツ専門学校、横浜リハビリテーション専門学校、横浜保育福祉専門学校、横浜実践看護専門学校
+            </div>
+          </li>
+        </ul>
+      </li>
+      <!-- 日本学生支援機構奨学生 -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">日本学生支援機構奨学生</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          経済的理由により修学に困難がある学生に対し貸与されます。高校在籍時に申込みをする予約採用と、入学後に申込みをする在学採用があります。
+        </div>
+      </li>
+      <!-- 教育ローン -->
+      <li class="<?php echo $pageName;?>__plan-item">
+        <div class="<?php echo $pageName;?>__plan-ttl">教育ローン</div>
+        <div class="<?php echo $pageName;?>__plan-txt">
+          教育ローンを利用することで、入学前に学費を用意できます。返済については、在学中は利息のみを支払い元金を据え置く「元金据え置き型分割払い」、在学中は保護者が、卒業後は本人が返済を行う「親子リレー返済方式」とれる教育ローンもあります。国の金融ローンである日本政策金融公庫、岩崎学園が銀行・信販会社と提携している教育ローンをご紹介いたします。
+        </div>
+      </li>
+    </ul>
+    <div class="<?php echo $pageName;?>__plan-system">
+      <div class="<?php echo $pageName;?>__plan-system-heading">２つの特待生制度</div>
+      <div class="<?php echo $pageName;?>__plan-system-intro">
+        特待生制度は、意欲的かつ将来の目的を明確にもって本学園に入学を希望する学生に対して、授業料を免除し学業を奨励する制度です。発想力を活かし、学園を盛り上げたいという積極的な学生を応援します。
+      </div>
+      <ul class="<?php echo $pageName;?>__plan-system-list">
+        <li class="<?php echo $pageName;?>__plan-system-item">
+          <div class="<?php echo $pageName;?>__plan-system-num">1</div>
+          <div class="<?php echo $pageName;?>__plan-system-ttl">特待生入学制度</div>
+          <div class="<?php echo $pageName;?>__plan-system-txt">
+            入学前の努力や実績を評価する制度です。<br>
+            例えば、資格取得、コンテスト入賞、部活動、委員会活動などでの実績や活躍の評価、また、高校での皆勤やボランティア活動をはじめとする努力を評価し、学費減免などの優遇措置を受けられる入学方法です。
+          </div>
+          <ul class="<?php echo $pageName;?>__plan-system-subList">
+            <li class="<?php echo $pageName;?>__plan-system-subItem">
+              <div class="<?php echo $pageName;?>__plan-system-subTtl">募集期間</div>
+              <div class="<?php echo $pageName;?>__plan-system-subTxt">願書提出時</div>
+            </li>
+            <li class="<?php echo $pageName;?>__plan-system-subItem">
+              <div class="<?php echo $pageName;?>__plan-system-subTtl">実施校</div>
+              <div class="<?php echo $pageName;?>__plan-system-subTxt">
+                情報科学専門学校、横浜医療情報専門学校、横浜ｆカレッジ、横浜デジタルアーツ専門学校、横浜リハビリテーション専門学校、横浜保育福祉専門学校、横浜実践看護専門学校
+              </div>
+            </li>
+          </ul>
+        </li>
+        <li class="<?php echo $pageName;?>__plan-system-item">
+          <div class="<?php echo $pageName;?>__plan-system-num">2</div>
+          <div class="<?php echo $pageName;?>__plan-system-ttl">内部特待生制度</div>
+          <div class="<?php echo $pageName;?>__plan-system-txt">
+            他の学生の模範となり得る方（学園横断・学内イベント、学校行事、学外コンテストへの参加、上位・難関企業へのチャレンジ、活動の外部発信、学習姿勢、学校の魅力向上等）を、先行投資型で募集する制度です。授業料の一部（40万円）が減免されます。
+          </div>
+          <ul class="<?php echo $pageName;?>__plan-system-subList">
+            <li class="<?php echo $pageName;?>__plan-system-subItem">
+              <div class="<?php echo $pageName;?>__plan-system-subTtl">募集期間</div>
+              <div class="<?php echo $pageName;?>__plan-system-subTxt">進級時</div>
+            </li>
+            <li class="<?php echo $pageName;?>__plan-system-subItem">
+              <div class="<?php echo $pageName;?>__plan-system-subTtl">実施校</div>
+              <div class="<?php echo $pageName;?>__plan-system-subTxt">
+                情報科学専門学校、横浜医療情報専門学校、横浜ｆカレッジ、横浜デジタルアーツ専門学校、横浜リハビリテーション専門学校 、横浜保育福祉専門学校、横浜実践看護専門学校
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- 学生寮 -->
+  <div class="<?php echo $pageName;?>__dormitory">
+      <h2 class="<?php echo $pageName;?>__dormitory-heading page-heading"><span>学生寮</span></h2>
+      <div class="<?php echo $pageName;?>__dormitory-intro">
+        本学園に入学される方の中には、親元から離れてひとり暮らしをしながら通学している学生も多数在籍しています。横浜は、みなとみらい・中華街などの観光地も有名ですが、家賃・物価もお手ごろで、交通の便がよく、海や豊かな自然に恵まれた住みやすい街です。 遠方から入学をお考えの方に住まいのサポートを致します
+      </div>
+      <!-- 指定学生寮（女子）のご案内 -->
+      <div class="<?php echo $pageName;?>__dormitory-guide">
+        <h3 class="page-bracketsTtl"><span>指定学生寮（女子）のご案内</span></h3>
+        <div class="<?php echo $pageName;?>__dormitory-guide-intro">
+          本学園学生寮は笑い声の絶えない、お風呂・キッチン・トイレが共同型の住まいです。家賃や生活費を抑えたい方、学園の友達をたくさん作りたい方にオススメです。岩崎学園が運営している学生寮。価格を低く設定しております。
+        </div>
+        <div class="<?php echo $pageName;?>__dormitory-guide-group">
+          <div class="<?php echo $pageName;?>__dormitory-guide-name">東白楽寮</div>
+          <div class="<?php echo $pageName;?>__dormitory-guide-inner">
+          <div class="<?php echo $pageName;?>__dormitory-guide-left">
+            <div class="<?php echo $pageName;?>__dormitory-guide-left-main">
+              <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_guide01.jpg">
+            </div>
+            <ul class="<?php echo $pageName;?>__dormitory-guide-left-sub">
+              <li class="<?php echo $pageName;?>__dormitory-guide-left-sub-item current">
+                <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_guide01.jpg">
+              </li>
+              <li class="<?php echo $pageName;?>__dormitory-guide-left-sub-item">
+                <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_guide02.jpg">
+              </li>
+              <li class="<?php echo $pageName;?>__dormitory-guide-left-sub-item">
+                <img class="object_fit" src="<?php echo get_stylesheet_directory_uri();?>/img/page/support/img_guide03.jpg">
+              </li>
+            </ul>
+          </div>
+          <ul class="<?php echo $pageName;?>__dormitory-guide-right">
+            <li class="<?php echo $pageName;?>__dormitory-guide-right-item">
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-ttl">収容</div>
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-txt">女子〇〇名（募集人員数ではありません）</div>
+            </li>
+            <li class="<?php echo $pageName;?>__dormitory-guide-right-item">
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-ttl">寮費＋維持費</div>
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-txt">41,000円～45,000円／1カ月（部屋の位置・広さによる）</div>
+            </li>
+            <li class="<?php echo $pageName;?>__dormitory-guide-right-item">
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-ttl">居室</div>
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-txt">個室 約5帖（バス・トイレ付き）</div>
+            </li>
+            <li class="<?php echo $pageName;?>__dormitory-guide-right-item">
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-ttl">設備</div>
+              <div class="<?php echo $pageName;?>__dormitory-guide-right-txt">
+                全室個室、無料Wi-Fi完備、大浴場、トイレ、自習室、自炊コーナー、冷蔵庫、電子レンジ、シューズロッカー、エアコン、テレビ、机、椅子、クローゼット、洗面台、トイレ、バスルーム
+              </div>
+            </li>
+          </div>
+        </div>
+      </div>
+      <!-- 学生寮3つのポイント -->
+      <div class="<?php echo $pageName;?>__dormitory-point">
+        <h3 class="page-bracketsTtl"><span>学生寮3つのポイント</span></h3>
+        <ul class="<?php echo $pageName;?>__dormitory-point-list">
+          <!-- 01 -->
+          <li class="<?php echo $pageName;?>__dormitory-point-item">
+            <div class="<?php echo $pageName;?>__dormitory-point-left">
+              <div class="<?php echo $pageName;?>__dormitory-point-num"><span>1</span></div>
+              <img class="object_fit" src="<?php //echo get_stylesheet_directory_uri();?>/img/page/support/img_guide01.jpg">
+            </div>
+            <div class="<?php echo $pageName;?>__dormitory-point-right">
+              <div class="<?php echo $pageName;?>__dormitory-point-ttl">管理人さんと友達との生活</div>
+              <div class="<?php echo $pageName;?>__dormitory-point-txt">
+                寮母さんと学生さんが和気あいあいと楽しく生活しています。親元を離れて心細い気持ちは皆同じ。入寮者は本学園の学生に限られるため、すぐに仲良くなれます。
+              </div>
+            </div>
+          </li>
+          <!-- 02 -->
+          <li class="<?php echo $pageName;?>__dormitory-point-item">
+            <div class="<?php echo $pageName;?>__dormitory-point-left">
+              <div class="<?php echo $pageName;?>__dormitory-point-num"><span>2</span></div>
+              <img class="object_fit" src="<?php //echo get_stylesheet_directory_uri();?>/img/page/support/img_guide01.jpg">
+            </div>
+            <div class="<?php echo $pageName;?>__dormitory-point-right">
+              <div class="<?php echo $pageName;?>__dormitory-point-ttl">駅徒歩３分、便利で安心</div>
+              <div class="<?php echo $pageName;?>__dormitory-point-txt">
+                東白楽駅徒歩3分、横浜駅へは電車で4分で定期代は1ヶ月1800円（学割）という好立地にあります。駅からちょっと坂を上りますが、高台から望む横浜の景色はとってもキレイです。
+              </div>
+            </div>
+          </li>
+          <!-- 03 -->
+          <li class="<?php echo $pageName;?>__dormitory-point-item">
+            <div class="<?php echo $pageName;?>__dormitory-point-left">
+              <div class="<?php echo $pageName;?>__dormitory-point-num"><span>3</span></div>
+              <img class="object_fit" src="<?php //echo get_stylesheet_directory_uri();?>/img/page/support/img_guide01.jpg">
+            </div>
+            <div class="<?php echo $pageName;?>__dormitory-point-right">
+              <div class="<?php echo $pageName;?>__dormitory-point-ttl">便利な共用設備と快適な居室</div>
+              <div class="<?php echo $pageName;?>__dormitory-point-txt">
+                共用設備として、大浴場やトイレ、洗濯機、自炊コーナーにコンロ、大型冷蔵庫、オーブンレンジ、ホール（食堂）にテレビがあります。個室にはカーテンと服をかける場所がついているので、お引越しの際は荷物も少なく済み、すぐに生活を始められます。
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="<?php echo $pageName;?>__dormitory-guidance">
+        <ul class="<?php echo $pageName;?>__dormitory-guidance-list">
+          <!-- 学生会館のご案内 -->
+          <li class="<?php echo $pageName;?>__dormitory-guidance-item">
+            <div class="<?php echo $pageName;?>__dormitory-guidance-left">
+              <h3 class="page-bracketsTtl"><span>学生会館のご案内</span></h3>
+            </div>
+            <div class="<?php echo $pageName;?>__dormitory-guidance-right">
+              家具や食事が準備される学生専用の住まいです。Uターン就職をお考えの方、食事の栄養面が心配な方におススメです。
+            </div>
+          </li>
+          <!-- 学生アパート・マンションのご案内 -->
+          <li class="<?php echo $pageName;?>__dormitory-guidance-item">
+            <div class="<?php echo $pageName;?>__dormitory-guidance-left">
+              <h3 class="page-bracketsTtl"><span>学生アパート・<br>マンションのご案内</span></h3>
+            </div>
+            <div class="<?php echo $pageName;?>__dormitory-guidance-right">
+              市内のワンルームマンションは6万円前後が相場です。家具付や学生専用、女性専用などさまざまなタイプの住まいをご紹介できます。岩崎学園入学希望の方への特典や割引もあります。
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="<?php echo $pageName;?>__dormitory-cv">
+        <div class="<?php echo $pageName;?>__dormitory-cv-txt">各種制度・学生寮については各校へお問い合わせください</div>
+        <a href="<?php echo home_url('/about/history'); ?>" class="page-group__contBtn">問い合わせる</a>
+      </div>
+  </div>
+  <div class="mod-banner">
+    <div class="mod-banner__ttl"><span>充実の設備・施設</span></div>
+    <a href="" target="_blank" class="page-group__contBtn">詳しく見る</a>
+  </div>
+</section>
--- js/scripts.js
+++ js/scripts.js
@@ -127,3 +127,22 @@ $(function(){
 		$(this).next('.page-career__achievement-answer').slideToggle();
 	});
 });
+
+// 学生支援
+$(function () {
+  $(".page-support__dormitory-guide-left-sub-item img").on("click", function () {
+    // メイン画像に切り替えるimgのsrc取得
+    img = $(this).attr("src");
+    // currentクラス付け替え(枠線などを変えたい時に)
+    $(".page-support__dormitory-guide-left-sub-item").removeClass("current");
+    $(this).parent().addClass("current");
+    // fadeOutできたらsrc変更してfadeIn
+    $(".page-support__dormitory-guide-left-main img").fadeOut(250, function () {
+      $(".page-support__dormitory-guide-left-main img")
+        .attr("src", img)
+        .on("load", function () {
+          $(this).fadeIn(250);
+        });
+    });
+  });
+});
