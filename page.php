2347006bef825525ba334ec4e81cc6fa66364359
柴田昂太朗 <kotaroshiba@heads-rep.com>
2024-11-21 16:41:45 +0900

    固定ページ、デフォルトエディターに値が入っているか調べるコードに変更

--- page.php
+++ page.php
@@ -1,15 +1,21 @@
 <?php
+// 現在の固定ページIDを取得
+$page_id = get_the_ID();
+
+// 固定ページの投稿オブジェクトを取得
+$page = get_post($page_id);
+
 get_header();
 if (have_posts()) {
 	while (have_posts()) {
 		the_post();
-		if ('' !== locate_template('content/page/'.get_page_uri().'.php')) {
-			get_template_part('content/page/'.get_page_uri());
-		} else {
+		if ($page && !empty(trim($page->post_content))) {
 			get_template_part('content/page');
+		} elseif('' !== locate_template('content/page/'.get_page_uri().'.php')) {
+			get_template_part('content/page/'.get_page_uri());
 		}
 	}
-	 ?>
+	?>
 	<?php if (function_exists('wp_pagenavi')) {
 		wp_pagenavi();
 	} ?>
