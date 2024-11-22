951ad9ffdbe32d01e922e6ae556bff798e696fbd
柴田昂太朗 <kotaroshiba@heads-rep.com>
2024-11-22 10:59:00 +0900

    固定ページ、テンプレートファイルの修正

--- content/page.php
+++ content/page.php
@@ -5,5 +5,9 @@
 ?>
 
 <!-- <div class="editor-content"> -->
-	<?php the_content(); ?>
+	<?php
+		remove_filter('the_content', 'wpautop'); // 自動<p>タグ挿入を無効化
+		the_content();
+		add_filter('the_content', 'wpautop');
+	?>
 <!-- </div> -->
--- page.php
+++ page.php
@@ -1,18 +1,14 @@
 <?php
-// 現在の固定ページIDを取得
-$page_id = get_the_ID();
-
-// 固定ページの投稿オブジェクトを取得
-$page = get_post($page_id);
-
 get_header();
 if (have_posts()) {
 	while (have_posts()) {
 		the_post();
-		if ($page && !empty(trim($page->post_content))) {
+		if (!empty(trim(get_the_content()))) {
 			get_template_part('content/page');
 		} elseif('' !== locate_template('content/page/'.get_page_uri().'.php')) {
 			get_template_part('content/page/'.get_page_uri());
+		} else {
+			get_template_part('content/page');
 		}
 	}
 	?>
