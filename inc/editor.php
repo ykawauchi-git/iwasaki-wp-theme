916962e9e0044e6ed36e5071df5168b3caf510fd
柴田昂太朗 <kotaroshiba@heads-rep.com>
2024-12-19 18:22:16 +0900

    KV画像 & 自動整形無効化の追記

--- inc/editor.php
+++ inc/editor.php
@@ -1,10 +1,48 @@
 <?php
+//自動整形無効化
 function custom_remove_wpautop() {
   if (is_front_page() || is_home() || is_page()) {
     remove_filter('the_content', 'wpautop');
   }
 }
 add_action('wp', 'custom_remove_wpautop');
+
+// 固定ページの編集画面でのみ自動整形を無効化
+add_action('init', function () {
+  add_filter('the_content', 'disable_wpautop_for_pages', 9);
+  add_filter('content_save_pre', 'disable_wpautop_for_pages_on_save', 9);
+});
+
+function disable_wpautop_for_pages($content) {
+  if (is_admin() && get_post_type() === 'page') {
+      // フロントエンドとエディタで整形を無効化
+      remove_filter('the_content', 'wpautop');
+  }
+  return $content;
+}
+
+function disable_wpautop_for_pages_on_save($content) {
+  if (is_admin() && get_post_type() === 'page') {
+      // 保存時の整形を無効化
+      remove_filter('content_save_pre', 'wpautop');
+  }
+  return $content;
+}
+
+// クラシックブロックの自動整形を無効化
+add_filter('tiny_mce_before_init', function ($initArray) {
+  // 自動整形を無効化
+  $initArray['wpautop'] = false;
+  $initArray['forced_root_block'] = false;
+  return $initArray;
+});
+
+// クラス名を保持する
+add_filter('tiny_mce_before_init', function ($init) {
+  // クラス属性を保持
+  $init['extended_valid_elements'] = '*[*]';
+  return $init;
+});
 
 function enable_default_gallery_style() {
   add_filter('use_default_gallery_style', '__return_true');
@@ -30,3 +68,4 @@ function disable_visual_editor_for_post_types($can_edit, $post) {
   return $can_edit;
 }
 add_filter('use_block_editor_for_post', 'disable_visual_editor_for_post_types', 10, 2);
+
